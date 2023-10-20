6a564a7ca0fccbe835f52f2aa3eda709fd666371
Valence Andre <valence@MacBook-Air-de-Valence.local>
2023-10-20 12:33:45 +0200

    first commit

---
+++ src/ft_strjoin.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vandre <vandre@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/12 12:48:16 by vandre            #+#    #+#             */
+/*   Updated: 2023/10/19 14:34:40 by vandre           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	len_s1;
+	size_t	len_s2;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	str = (char *)malloc(sizeof(char) * (len_s1 + len_s2 + 1));
+	if (!str)
+		return (NULL);
+	ft_strlcpy(str, s1, len_s1 + 1);
+	ft_strlcat(str + len_s1, s2, len_s2 + 1);
+	return (str);
+}
+/*
+int	main(void)
+{
+	char s1[] = "Hello";
+	char s2[] = "World";
+	char	*str;
+
+	str = ft_strjoin(s1, s2);
+	if (!str)
+		return (1);
+
+	printf("%s\n", str);
+	return (0);
+}*/
